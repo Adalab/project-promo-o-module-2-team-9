9f3cd3f74682531141082c889437df763c77bfb6
Rocío Dols Pividal <rdolspividal@gmail.com>
2021-12-10 14:11:50 +0100

    AddChanges

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('>> Ready :)');
+
+//Arrow
+const arrowElement = document.querySelector('.js_arrow');
+
+const fillContainer = document.querySelector('.js_fillContainer');
+
+const formHeader = document.querySelector('.js_formHeader');
+
+function handleClickHeader() {
+    fillContainer.classList.toggle('js_collapsed');
+    arrowElement.classList.toggle('js_arrowRotate');
+}
+
+
+
+
+
+
+formHeader.addEventListener('click', handleClickHeader);
