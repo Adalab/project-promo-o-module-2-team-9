e5d0650eee0047199380ca95aa9a30f7f30b96ec
Ico Lizhen <icolizhen@gmail.com>
2021-12-11 21:51:18 +0000

    fix footer in tablet and add correct arrow rotation

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@
 
 
 //Arrow
-const arrowElement = document.querySelector('.js_arrow');
+const arrowFill = document.querySelector('.js_arrowFill');
 const fillContainer = document.querySelector('.js_fillContainer');
 const formHeader = document.querySelector('.js_formHeader');
 
 function handleClickHeader() {
   fillContainer.classList.toggle('js_collapsed');
-  arrowElement.classList.toggle('js_arrowRotate');
+  arrowFill.classList.toggle('js_arrowRotate');
 }
 
 formHeader.addEventListener('click', handleClickHeader);
