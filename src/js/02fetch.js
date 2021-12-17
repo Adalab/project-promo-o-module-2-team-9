4ab26ecfd2b1fe0cf28ecf53b721ad9e675fcbd2
Ico Lizhen <icolizhen@gmail.com>
2021-12-17 12:29:55 +0000

    refresh

--- src/js/02fetch.js
+++ src/js/02fetch.js
@@ -5,12 +5,11 @@ const createButton = document.querySelector('.js_create_button');
 function handleCreateCard(event) {
   event.preventDefault();
 
-  fetch('https://awesome-profile-cards.herokuapp.com/card',
-    {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {'Content-Type': 'application/json'}
-    })
+  fetch('https://awesome-profile-cards.herokuapp.com/card', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json' },
+  })
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
