535ffc07f5867eaf24aa8dc04f650ab5fd8ad727
Rocío Dols Pividal <rdolspividal@gmail.com>
2021-12-20 23:54:44 +0100

    add and remove colors PreviewCard

--- src/js/01main.js
+++ src/js/01main.js
@@ -5,6 +5,11 @@ const designHeader = document.querySelector('.js_designHeader');
 const formHeader = document.querySelector('.js_formHeader');
 const shareHeader = document.querySelector('.js_shareHeader');
 
+//Recogemos el div del color
+const cardDecoration = document.querySelector('.card__decoration');
+const borderIcons = document.querySelectorAll('.js_icon_border');
+const imageIcons = document.querySelectorAll('.js_icon_image');
+
 function handleClickHeader(event) {
   event.currentTarget.nextElementSibling.classList.toggle('js_collapsed');
   event.currentTarget.lastElementChild.classList.toggle('js_arrowRotate');
@@ -78,11 +83,54 @@ for (const eachPalette of allPalettes) {
 function handleClickPalette(ev) {
   const paletteValue = parseInt(ev.currentTarget.value);
 
+  //Borro todas las clases porque no se cual tenia
+  limpiaClases();
+
   if (paletteValue === 1) {
-    console.log('add class css 1');
+    cardDecoration.classList.add('card__decoration__palette1');
+    modificaSocialIcons('social__icon__palette1');
+    modificaIconsImages('icon_image__pallete1');
   } else if (paletteValue === 2) {
-    console.log('add class css 2');
+    cardDecoration.classList.add('card__decoration__palette2');
+    modificaSocialIcons('social__icon__palette2');
+    modificaIconsImages('icon_image__pallete2');
   } else if (paletteValue === 3) {
-    console.log('add class css 3');
+    cardDecoration.classList.add('card__decoration__palette3');
+    modificaSocialIcons('social__icon__palette3');
+    modificaIconsImages('icon_image__pallete3');
+  }
+}
+//Clases que le dan color al div, es sólo un elemento
+function limpiaClases() {
+  cardDecoration.classList.remove('card__decoration__palette1');
+  cardDecoration.classList.remove('card__decoration__palette2');
+  cardDecoration.classList.remove('card__decoration__palette3');
+  //Borde de los iconos
+  for (const icon of borderIcons) {
+    icon.classList.remove('social__icon__palette1');
+    icon.classList.remove('social__icon__palette2');
+    icon.classList.remove('social__icon__palette3');
+  }
+  //Las imagenes
+  for (const image of imageIcons) {
+    image.classList.remove('icon_image__pallete1');
+    image.classList.remove('icon_image__pallete2');
+    image.classList.remove('icon_image__pallete3');
+  }
+
+}
+
+function modificaSocialIcons(clase) {
+  //Recorremos todos los li para ponerles la clase
+  for (const icon of borderIcons) {
+    icon.classList.add(clase);
+  }
+}
+
+//Funcion para los colores de los iconos 
+
+function modificaIconsImages(clase) {
+  for (const image of imageIcons) {
+    image.classList.add(clase);
   }
 }
